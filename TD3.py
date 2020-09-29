d868a350f7f0cdd5ab7b2ce24d4ed11f08ddcea9
thxsxth <50850495+thxsxth@users.noreply.github.com>
2020-09-28 20:10:14 -0400

    Add files via upload

---
+++ TD3.py
@@ -0,0 +1,77 @@
+import torch
+import numpy as np
+import pandas as pd
+import datetime as dt
+import random
+import time
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset,DataLoader
+import torch.nn.functional as F
+import os
+import glob
+import copy
+
+device='cuda' if torch.cuda.is_available() else 'cpu'
+
+"""
+Implements TD3 code taken modified (slightly) from https://github.com/sfujim/TD3/blob/master/TD3.py
+
+"""
+
+class Actor(nn.Module):
+	def __init__(self, state_dim, action_dim, max_action):
+		super(Actor, self).__init__()
+
+		self.l1 = nn.Linear(state_dim, 256)
+		self.l2 = nn.Linear(256, 256)
+		self.l3 = nn.Linear(256, action_dim)
+		
+		self.max_action = max_action
+		
+
+	def forward(self, state):
+		a = F.relu(self.l1(state))
+		a = F.relu(self.l2(a))
+		return self.max_action * torch.tanh(self.l3(a))
+
+
+
+class Critic(nn.Module):
+	def __init__(self, state_dim, action_dim):
+		super(Critic, self).__init__()
+
+		# Q1 architecture
+		self.l1 = nn.Linear(state_dim + action_dim, 256)
+		self.l2 = nn.Linear(256, 256)
+		self.l3 = nn.Linear(256, 1)
+
+		# Q2 architecture
+		self.l4 = nn.Linear(state_dim + action_dim, 256)
+		self.l5 = nn.Linear(256, 256)
+		self.l6 = nn.Linear(256, 1)
+
+
+	def forward(self, state, action):
+		sa = torch.cat([state, action], 1)
+
+		q1 = F.relu(self.l1(sa))
+		q1 = F.relu(self.l2(q1))
+		q1 = self.l3(q1)
+
+		q2 = F.relu(self.l4(sa))
+		q2 = F.relu(self.l5(q2))
+		q2 = self.l6(q2)
+		return q1, q2
+
+
+	def Q1(self, state, action):
+		sa = torch.cat([state, action], 1)
+
+		q1 = F.relu(self.l1(sa))
+		q1 = F.relu(self.l2(q1))
+		q1 = self.l3(q1)
+		return q1
+
+
+		tau=0.005,
