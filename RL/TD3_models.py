ccc438566c73505ff7aa664504eaf1c6e1891601
thxsxth <50850495+thxsxth@users.noreply.github.com>
2020-10-01 22:21:40 -0400

    Update TD3_models.py

--- RL/TD3_models.py
+++ RL/TD3_models.py
@@ -73,5 +73,104 @@ class Critic(nn.Module):
 		q1 = self.l3(q1)
 		return q1
 
+	
 
+
+class TD3(object):
+ def __init__(
+		self,
+		state_dim,
+		action_dim,
+		max_action,
+		discount=0.99,
 		tau=0.005,
+		policy_noise=0.2,
+		noise_clip=0.5,
+		policy_freq=2
+	):
+
+		self.actor = Actor(state_dim, action_dim, max_action).to(device)
+		self.actor_target = copy.deepcopy(self.actor)
+		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+		self.critic = Critic(state_dim, action_dim).to(device)
+		self.critic_target = copy.deepcopy(self.critic)
+		self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+
+		self.max_action = max_action
+		self.discount = discount
+		self.tau = tau
+		self.policy_noise = policy_noise
+		self.noise_clip = noise_clip
+		self.policy_freq = policy_freq
+
+		self.total_it = 0
+
+
+ def select_action(self, state):
+		state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+		return self.actor(state).cpu().data.numpy().flatten()
+
+
+ def train_epoch(self, data_loader,it):
+    # self.total_it += 1
+    critic_sum_loss=0
+    actor_sum_loss=0
+
+	
+    for batch,(states,next_states,actions,reward,done) in enumerate(data_loader):
+      batch_size=states.shape[0]
+      reward=reward.to(device).reshape(batch_size,-1)
+      done=done.to(device).reshape(batch_size,-1)
+
+      with torch.no_grad():
+        noise = (
+				torch.randn_like(actions) * self.policy_noise).clamp(-self.noise_clip, self.noise_clip)
+        next_actions = (
+				self.actor_target(next_states) + noise).clamp(-self.max_action, self.max_action)
+        
+        target_Q1, target_Q2 = self.critic_target(next_states, next_actions)
+        target_Q=torch.min(target_Q1, target_Q2)
+        target_Q = reward + (1-done) * self.discount * target_Q
+
+        # Hack to get rid of a double dtype error
+        target_Q=torch.FloatTensor(target_Q.cpu().numpy()).to(device)    
+
+      current_Q1, current_Q2 = self.critic(states, actions)
+
+      # Compute critic loss
+      critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+      critic_sum_loss+=critic_loss.item()
+
+      if batch%50==0:
+        print("Epoch :",it,'Batch :', batch, 'Average Critic Loss :',critic_sum_loss/(batch+1))
+
+      self.critic_optimizer.zero_grad()
+      critic_loss.backward()
+      self.critic_optimizer.step()
+
+      if batch % self.policy_freq == 0:
+        actor_loss = -self.critic.Q1(states, self.actor(states)).mean()
+        if batch%25==0:
+            print("Epoch :",it,'Batch :', batch, 'Actor Loss for this batch:',actor_loss.item())
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+          target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        
+				    
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+		
+
+     
+			  
+   
+      
+   
+		
+		
